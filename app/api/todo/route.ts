828f443df97048d62f4c66187db7c2baee6b6d99
depermana12 <deddiapermana97@gmail.com>
2024-07-01 19:47:48 +0700

    next route handler, serverless for free cool

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,6 +1,19 @@
 import { NextResponse } from "next/server";
+import db from "../../../utils/db";
 
 // eslint-disable-next-line no-unused-vars
 export const GET = async (request: Request) => {
-    return NextResponse.json({ message: "hello darknest my old friend" });
+  const todos = await db.todo.findMany({});
+  return NextResponse.json({
+    success: true,
+    length: todos.length,
+    data: todos,
+  });
+};
+export const POST = async (request: Request) => {
+  const data = await request.json();
+  const todo = await db.todo.create({
+    data,
+  });
+  return NextResponse.json({ message: todo });
 };
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 
 export default function Page() {
-    return (
-        <main>
-            <Link href="/users" target="_blank" rel="noopener noreferrer">
-                USERS
-            </Link>
-            <ProductCard />
-        </main>
-    );
+  return (
+    <main>
+      <Link href="/users" target="_blank" rel="noopener noreferrer">
+        USERS
+      </Link>
+      <ProductCard />
+    </main>
+  );
 }
